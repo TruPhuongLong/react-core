fd042229a8b639dc51ea6216eb3c1922cd812db6
truphuonglong <longbaloca@gmail.com>
2019-03-03 10:53:22 +0700

    send mail

--- src/components/normals/learn-html.js
+++ src/components/normals/learn-html.js
@@ -7,5 +7,8 @@ export default _ => (
         <a href="mailto:longbaloca@gmail.com">send mail</a>
         <br />
         <a href="tel:+840383884519">call mother</a>
+        <br />
+        You can populate the subject and body for the new email as well:
+<a href="mailto:longbaloca@gmail.com?subject=Example+subject&body=Message+text">Send email already have body</a>
     </div>
 )
